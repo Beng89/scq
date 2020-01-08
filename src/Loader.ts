7362ff0a335c9ca0776583e0f39a1d05887346d6
Ben Goltz <ben.goltz@outlook.com>
2020-01-07 22:02:05 -0600

    Added ability to filter loaded files.

--- src/Loader.ts
+++ src/Loader.ts
@@ -1,8 +1,19 @@
 import { readdirSync } from "fs"
 import { join } from "path"
 
-export const loadHandlers = ({ directory = "" }) => {
+type Options = {
+  directory: string
+
+  filter?: (file: string) => boolean
+}
+export const loadHandlers = (options: Options) => {
+  let { directory, filter } = options
+
+  filter = typeof filter === "function" ? filter
+    : () => true
+
   const files = readdirSync(directory)
+    .filter(filter)
 
   files.forEach(file => require(join(directory, file)))
 }
